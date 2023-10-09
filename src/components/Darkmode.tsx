8ba72775af9a53a8cfd7520b4dbeb84c7132924f
Angel Darco <angel92-eme@hotmail.com>
2023-10-09 01:33:59 -0500

    :zap: darkmode styles and fucntionality inplemented

--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -1,32 +1,48 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import styles from "./darkmode.module.css";
-import { customStyleProps, themeType } from "./types";
+import moduleStyles from "./darkmode.module.css";
+import { darkmodeProps, themeType } from "./types";
 import DarkMode from "./DarkMode";
 
-type darkmodeProps = {
-  switchStyles?: customStyleProps;
-  icons: object[]
-}
-
 const Darkmode = (props: darkmodeProps) => {
-const { switchStyles, icons } = props;
+const { switchStyles, icons, switchMode, styles } = props;
 const darkmode = new DarkMode();
 
   const [theme, setTheme] = useState<themeType>();
   const [switchTheme, setSwitchTheme] = useState(false);
+  const [icon, setIcon] = useState<JSX.Element>();
 
   useEffect(() => {
     // recover theme storage.
     const storagedTheme = darkmode.storageDarkmodePreference();
     
-    if (!storagedTheme && !theme) {setTheme(darkmode.detectUserTheme());
-    }else if(storagedTheme && !theme){ setTheme(storagedTheme);
-    }else
+    // detect user theme in the browser and localStorage
+    if (!storagedTheme && !theme) setTheme(darkmode.detectUserTheme());
+    else if(storagedTheme && !theme) setTheme(storagedTheme);
+    else
     if(theme){
     if (switchStyles) darkmode.changePropsStyles(theme, switchStyles);
-    else darkmode.changeDefaultStyles(theme);
+    else darkmode.changeMode(theme);
+
+    // set theme in localStorage
     darkmode.storageDarkmodePreference(theme);
+
+    // set icons
+    if(icons && theme && typeof icons !== "boolean")
+    setIcon(darkmode.handlerIcons(theme, icons));
+
+  // position switch theme
+  if(theme === "Dark") setSwitchTheme(true);
+  }
+
+  // default values 
+  if(theme && !switchStyles) {
+    const defaultStyles = darkmode.handlerDefaultSwitchTheme().defaultStyles;
+    darkmode.changePropsStyles(theme, defaultStyles);
+  }
+  if(theme && typeof icons === "boolean") {
+    const defaultIcons = darkmode.handlerDefaultSwitchTheme().icons;
+    setIcon(darkmode.handlerIcons(theme, defaultIcons));
   }
 }, [theme]);
 
@@ -35,21 +51,28 @@ const darkmode = new DarkMode();
     if (theme) setTheme((prev) => (prev === "Light" ? "Dark" : "Light"));
 
     setSwitchTheme((prev) => !prev);
-    darkmode.handlerIcons(icons);
 
+    // handler icons
+    if(icons && theme && typeof icons !== "boolean")
+    setIcon(darkmode.handlerIcons(theme, icons));
 
     if(theme){
       if (switchStyles) darkmode.changePropsStyles(theme, switchStyles);
-      else darkmode.changeDefaultStyles(theme);
+      else darkmode.changeMode(theme);
   }
   };
 
   return (
-    <div className={`${styles.darkmodeContainer} ${switchTheme ? styles.switchContainerAnimation : ""}`} onClick={handlerSwitchTheme}>
+    <div style={styles?.mainContainer} className={`${moduleStyles.darkmodeContainer} 
+    ${
+      switchMode === "static" ? moduleStyles.staticSwitch : switchTheme ? moduleStyles.dinamicSwitch : moduleStyles.normalSwitch
+    }`} onClick={handlerSwitchTheme}>
 
-      <button className={styles.switch}>
-        {theme}
-      </button>
+      <div style={styles?.switchContainer} className={`${moduleStyles.switch} ${!icon ? moduleStyles.addBorder : ""}`}>
+      <div style={styles?.iconsContainer} className={moduleStyles.svg}>
+      {icon ? icon : theme}
+      </div>
+      </div>
     </div>
   );
 };
