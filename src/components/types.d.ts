2d3f57699497e414fd875a4824d47d6ca4a0a962
Angel Darco <angel92-eme@hotmail.com>
2023-10-09 01:31:45 -0500

    :sparkles: default types and variables added

--- src/components/types.d.ts
+++ src/components/types.d.ts
@@ -1,5 +1,21 @@
+import { CSSProperties } from "react";
+
+export type customStylesProps = {
+  mainContainer?: CSSProperties;
+  switchContainer?: CSSProperties;
+  iconsContainer?: CSSProperties;
+};
+
+export type switchMod = "dinamic" | "static";
+export type darkmodeProps = {
+  switchStyles?: handlerStylesProps;
+  icons?: iconsType | boolean;
+  switchMode?: switchMod;
+  styles?: customStylesProps;
+};
+
 export type themeType = "Light" | "Dark";
-export type customStyleProps = [
+export type handlerStylesProps = [
   {
     theme: themeType;
     variables: string[];
@@ -9,5 +25,16 @@ export type customStyleProps = [
     theme: themeType;
     variables: string[];
     values: string[];
+  }
+];
+
+export type iconsType = [
+  {
+    theme: themeType;
+    icon: JSX.Element;
+  },
+  {
+    theme: themeType;
+    icon: JSX.Element;
   }
 ];
