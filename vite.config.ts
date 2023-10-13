3c08f9e7072591c51d32905b4c05c746446f5daa
Angel Darco <angel92-eme@hotmail.com>
2023-10-13 01:15:48 -0500

    :sparkles: vite config to deploy component updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build:{
+    lib:{
+      name: "index",
+      fileName: "index",
+      entry: "./src/components/Darkmode.tsx",
+      formats: ["es", "umd", "cjs"],
+    },
+    minify: true,
+    emptyOutDir: true,
+
+    rollupOptions:{
+      external: ['react', 'react-dom'],
+      output:{
+        dir: "dist",
+        globals:{
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
+  },
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+    dts({ rollupTypes: true })
+  ]
 })
