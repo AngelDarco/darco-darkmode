60f62550951757a5dfd83dc9d020bc9c573c3d90
Angel Darco <angel92-eme@hotmail.com>
2023-10-13 01:18:54 -0500

    :hammer: default props created

---
+++ src/utils/defaultProps.ts
@@ -0,0 +1,35 @@
+import type { switchStylesProps, iconsType } from "../types/types";
+import Icons from "../assets/Icons"
+
+const defaultProps = ()=> {
+  return {
+    defaultStyles,
+    icons
+  }
+}
+
+const defaultStyles: switchStylesProps = [
+    {
+      theme: "Dark",
+      variables: ["--main-background", "--main-color", "--main-hover-color"],
+      values: ["#000", "#fff", "#fbfbfb"],
+    },
+    {
+      theme: "Light",
+      variables: ["--main-background", "--main-color", "--main-hover-color"],
+      values: ["#fff", "#000", "#242526"],
+    },
+  ];
+
+  const { dark, light } = Icons();
+  const icons: iconsType = [
+    {
+      theme: "Dark",
+      icon: dark
+    },
+    {
+      theme: "Light",
+      icon: light
+    }
+  ];
+  export default defaultProps;
