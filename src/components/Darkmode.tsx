184e0aff9c76ac89d1b3d35d525adb0ab616bb15
Angel Darco <angel92-eme@hotmail.com>
2023-10-13 01:19:30 -0500

    :sparkles: component and class with logic and functionality created

--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import moduleStyles from "./darkmode.module.css";
-import { darkmodeProps, themeType } from "./types";
-import DarkMode from "./DarkMode";
+import moduleStyles from "../styles/darkmode.module.css";
+import type { darkmodeProps, themeType } from "../types/types";
+import DarkMode from "../utils/DarkMode";
 
 const Darkmode = (props: darkmodeProps) => {
-const { switchStyles, icons, switchMode, styles } = props;
-const darkmode = new DarkMode();
+  const { switchStyles, icons, switchMode, styles, hover } = props;
+  const darkmode = new DarkMode();
 
   const [theme, setTheme] = useState<themeType>();
   const [switchTheme, setSwitchTheme] = useState(false);
@@ -15,37 +15,35 @@ const darkmode = new DarkMode();
   useEffect(() => {
     // recover theme storage.
     const storagedTheme = darkmode.storageDarkmodePreference();
-    
+
     // detect user theme in the browser and localStorage
     if (!storagedTheme && !theme) setTheme(darkmode.detectUserTheme());
-    else if(storagedTheme && !theme) setTheme(storagedTheme);
-    else
-    if(theme){
-    if (switchStyles) darkmode.changePropsStyles(theme, switchStyles);
-    else darkmode.changeMode(theme);
+    else if (storagedTheme && !theme) setTheme(storagedTheme);
+    else if (theme) {
+      if (switchStyles) darkmode.changePropsStyles(theme, switchStyles);
+      else darkmode.changeMode(theme);
 
-    // set theme in localStorage
-    darkmode.storageDarkmodePreference(theme);
+      // set theme in localStorage
+      darkmode.storageDarkmodePreference(theme);
 
-    // set icons
-    if(icons && theme && typeof icons !== "boolean")
-    setIcon(darkmode.handlerIcons(theme, icons));
+      // set icons
+      if (icons && theme && typeof icons !== "boolean")
+        setIcon(darkmode.handlerIcons(theme, icons));
 
-  // position switch theme
-  if(theme === "Dark") setSwitchTheme(true);
-  }
+      // position switch theme
+      if (theme === "Dark") setSwitchTheme(true);
+    }
 
-  // default values 
-  if(theme && !switchStyles) {
-    const defaultStyles = darkmode.handlerDefaultSwitchTheme().defaultStyles;
-    darkmode.changePropsStyles(theme, defaultStyles);
-  }
-  if(theme && typeof icons === "boolean") {
-    const defaultIcons = darkmode.handlerDefaultSwitchTheme().icons;
-    setIcon(darkmode.handlerIcons(theme, defaultIcons));
-  }
-}, [theme]);
-
+    // default values
+    if (theme && !switchStyles) {
+      const defaultStyles = darkmode.handlerDefaultSwitchTheme().defaultStyles;
+      darkmode.changePropsStyles(theme, defaultStyles);
+    }
+    if (theme && typeof icons === "boolean") {
+      const defaultIcons = darkmode.handlerDefaultSwitchTheme().icons;
+      setIcon(darkmode.handlerIcons(theme, defaultIcons));
+    }
+  }, [theme]);
 
   const handlerSwitchTheme = () => {
     if (theme) setTheme((prev) => (prev === "Light" ? "Dark" : "Light"));
@@ -53,25 +51,38 @@ const darkmode = new DarkMode();
     setSwitchTheme((prev) => !prev);
 
     // handler icons
-    if(icons && theme && typeof icons !== "boolean")
-    setIcon(darkmode.handlerIcons(theme, icons));
+    if (icons && theme && typeof icons !== "boolean")
+      setIcon(darkmode.handlerIcons(theme, icons));
 
-    if(theme){
+      // handler switch mode
+    if (theme) {
       if (switchStyles) darkmode.changePropsStyles(theme, switchStyles);
       else darkmode.changeMode(theme);
-  }
+    }
   };
 
   return (
-    <div style={styles?.mainContainer} className={`${moduleStyles.darkmodeContainer} 
+    <div
+      style={styles?.mainContainer}
+      className={`${moduleStyles.darkmodeContainer} 
     ${
-      switchMode === "static" ? moduleStyles.staticSwitch : switchTheme ? moduleStyles.dinamicSwitch : moduleStyles.normalSwitch
-    }`} onClick={handlerSwitchTheme}>
-
-      <div style={styles?.switchContainer} className={`${moduleStyles.switch} ${!icon ? moduleStyles.addBorder : ""}`}>
-      <div style={styles?.iconsContainer} className={moduleStyles.svg}>
-      {icon ? icon : theme}
-      </div>
+      switchMode === "static"
+        ? moduleStyles.staticSwitch
+        : switchTheme
+        ? moduleStyles.dinamicSwitch
+        : moduleStyles.normalSwitch
+    }`}
+      onClick={handlerSwitchTheme}
+    >
+      <div
+        style={styles?.switchContainer}
+        className={`${moduleStyles.switch} ${
+          !icon ? moduleStyles.addBorder : ""
+        } ${hover ? moduleStyles.switchHover : ""}`}
+      >
+        <div style={styles?.iconsContainer} className={moduleStyles.svg}>
+          {icon ? icon : theme}
+        </div>
       </div>
     </div>
   );
---
+++ src/utils/DarkMode.ts
@@ -0,0 +1,106 @@
+import type { switchStylesProps, iconsType, themeType } from "../types/types";
+import defaultProps from "./defaultProps";
+
+export default class DarkMode {
+    /**
+   * Returns a MediaQueryList object representing the user's preference for dark theme.
+   *
+   * @return {MediaQueryList} The MediaQueryList object representing the user's preference for dark theme.
+   */
+  private userDarkTheme(): MediaQueryList {
+    return window.matchMedia("(prefers-color-scheme: dark)");
+  }
+
+    /**
+   * Applies the specified theme styles to the given set of elements.
+   *
+   * @param {themeType} theme - The theme to apply.
+   * @param {switchStylesProps} styles - The array with the dark and light objects of styles to apply.
+   * @return {void} This function does not return a value.
+   */
+  changePropsStyles(theme: themeType, styles: switchStylesProps): void {
+    const addStyles = (arrVariables: string[], arrValues: string[]) => {
+      for (let i = 0; i < arrVariables.length; i++) {
+        const variables = arrVariables[i];
+        const values = arrValues[i];
+
+        const root: HTMLDivElement | null = document.querySelector(":root");
+        root?.style.setProperty(variables, values);
+      }
+    };
+
+    const currentTheme: themeType = this.changeMode(theme);
+
+    const light = styles[0].theme === "Light" ? styles[0] : styles[1];
+    const dark = styles[0].theme === "Dark" ? styles[0] : styles[1];
+
+    if (currentTheme !== "Light") addStyles(light.variables, light.values);
+    else addStyles(dark.variables, dark.values);
+  }
+
+  /**
+   * Change the mode of the theme.
+   *
+   * @param {themeType} theme - The current theme mode.
+   * @return {themeType} The updated theme mode.
+   */
+  changeMode(theme: themeType): themeType {
+    if (theme !== "Dark") return "Dark";
+    else return "Light";
+  }
+
+    /**
+   * Detects the theme of the user.
+   *
+   * @return {themeType} The theme detected: "Dark" or "Light".
+   */
+  detectUserTheme(): themeType {
+    if (this.userDarkTheme().matches) return "Dark";
+    else return "Light";
+  }
+
+  /**
+   * Retrieves or sets the dark mode preference from local storage.
+   *
+   * @param {themeType} theme - The theme to set as the dark mode preference.
+   * @return {themeType | undefined} The dark mode preference retrieved from local storage, or undefined if no preference is set.
+   */
+  storageDarkmodePreference(theme?: themeType): themeType | undefined {
+    const storage = window.localStorage.getItem("dark-mode") ?? undefined;
+    if (!theme) return storage as themeType;
+    else window.localStorage.setItem("dark-mode", theme);
+  }
+
+  /**
+   * Handles the icons based on the theme.
+   *
+   * @param {themeType} theme - The theme type Light or Dark.
+   * @param {iconsType} icons - The object with the dark and light theme and the icons .
+   * @return {string | undefined} The icon based on the theme, or undefined if no matching icon is found.
+   */
+  handlerIcons(theme: themeType, icons?: iconsType) {
+    if (icons && theme) {
+      const darkTheme =
+        icons[0].theme === "Dark" ? icons[0].theme : icons[1].theme;
+      const lightTheme =
+        icons[1].theme === "Light" ? icons[1].theme : icons[0].theme;
+
+      const darkIcon = icons[0].icon;
+      const lightIcon = icons[1].icon;
+
+      if (theme === darkTheme) return darkIcon;
+      else if (lightTheme === theme) return lightIcon;
+      else return undefined;
+    }
+  }
+
+  /**
+ * A function that handles the default switch theme.
+ *
+ * @return {Object} An object containing the default styles and icons.
+ */
+  handlerDefaultSwitchTheme() {
+    const { defaultStyles, icons } = defaultProps();
+    return { defaultStyles, icons };
+  }
+}
